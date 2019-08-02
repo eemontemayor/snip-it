bcd1c296927dde43209728fd6b009eb738160840
Enrique Montemayor <eemontemayor@live.com>
2019-08-02 16:11:57 -0500

    x

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Button, Input, Textarea } from '../Utils/Utils';
+import { Button, Input, Textarea } from './Utils/Utils';
 import './App.css';
 
 class App extends Component {
@@ -12,27 +12,36 @@ class App extends Component {
     },()=>{
         console.log(this.state)
     })
+  }
 
+  handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(this.state)
   }
   render(){
 
     return (
       <div className="App">
       <header className="App-header">
-    
+      Snip It
       </header>
+      <form onSubmit={this.handleSubmit}>
       <div className='search-box'>
-              <label htmlFor='search-string'>
+              <label htmlFor='search'>
                 Search
               </label>
               <Input
                 type="text"
-                name='search-string'
+                name='searchString'
                 onChange={this.handleChange}
                 required
                 >
               </Input>
+              <Button type='submit'>
+
+              </Button>
             </div>
+            </form>
     </div>
   );
 }
