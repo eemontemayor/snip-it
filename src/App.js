fd7cf6fed84b1410c70276ef4571a43ccdd5450a
Enrique Montemayor <eemontemayor@live.com>
2019-08-20 11:35:15 -0500

    clear form func

--- src/App.js
+++ src/App.js
@@ -1,65 +1,71 @@
 import React,{Component} from 'react';
-import { Button, Input } from './Utils/Utils';
+import { Button, Input,Section } from './Utils/Utils';
 import './App.css';
 import Translations from './Component/Translations'
 import TranslationServices from './services/TranslationServices'
+import SearchStr from './Component/SearchStr';
 class App extends Component {
   state={
     searchString:'',
+    searchStr:'',
+  
     results:[]
   }
-  handleChange = (e) => {
-    this.setState({
-    [e.target.name]:e.target.value
-    },()=>{
-        console.log(this.state)
-    })
-  }
+      handleChange = (e) => {
+        this.setState({
+        [e.target.name]:e.target.value
+        },()=>{
+            console.log(this.state)
+        })
+      }
 
-  handleSubmit=(e)=>{
-    e.preventDefault()
-    TranslationServices.getTranslations(this.state.searchString)
-    .then(res => {
-      console.log('from App.js', res.body)
-      this.setState({
-        results:res.body.matches
-      },()=>{
-        console.log(this.state)
+      handleSubmit=(e)=>{
+        e.preventDefault()
+        let string= this.state.searchString
+        TranslationServices.getTranslations(this.state.searchString)
+        .then(res => {
+          console.log('from App.js', res.body)
+          this.setState({
+            results:res.body.matches,
+            searchString:'',
+            searchStr:string
+          },()=>{
+            console.log(this.state)
+          })
+          
       })
-      
-  })
-} 
+    } 
   render(){
 
     return (
       <div className="App">
-      <header className="App-header">
-      Snip It
-      </header>
-      <div className='search-box'>
-      <form onSubmit={this.handleSubmit}>
+        <header className="App-header">
+        Snip It
+        </header>
+        <div className='search-box'>
+          <form onSubmit={this.handleSubmit}>
               <label htmlFor='search'>
                 
               </label>
               <Input
                 type="text"
                 name='searchString'
+                value={this.state.searchString}
                 onChange={this.handleChange}
-                required
-                >
+                required>
               </Input>
               <div className='btn-holder'>
-
-              <Button className='search-btn'type='submit' >
-                Search
-              </Button>
+              <Button className='search-btn'type='submit' >Search</Button>
               </div>
-            </form>
-            </div>
-{    this.state.results  &&  <Translations results={this.state.results}/>}
-    </div>
-  );
-}
+          </form>
+        </div>
+        <Section >
+          {this.state.results && <> <SearchStr className='search-str' string={this.state.searchStr}/><Translations className='search-trans'results={this.state.results}/></>}
+        </Section>
+       
+      </div>
+      );
+    }
 }
 
 export default App;
---
+++ src/Component/SearchStr.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SearchStr(props){
+    return(
+        <div>
+            {props.string}
+        </div>
+    )
+}
+export default SearchStr
