d9e4756f95fbe88747d8e069a5420986057dc525
Enrique Montemayor <eemontemayor@live.com>
2019-08-20 11:46:55 -0500

    x

--- src/Component/SearchStr.js
+++ src/Component/SearchStr.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 function SearchStr(props){
+    console.log(props , 'from SearchStr comp')
+
+if(props.string === ''){
+    return(
+        <div>
+            search for a snippet in spanish to see translation
+        </div>
+    )
+}
+    else{
+
+    
     return(
         <div>
             {props.string}
         </div>
     )
+    }
 }
 export default SearchStr
